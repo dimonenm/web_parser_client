485918bfeda330d66f322edd0464616b29184e72
exp <arzuakov@yahoo.com>
2026-09-24 07:11:54 +0300

    24.09.2026

--- src/app/instagram/page.tsx
+++ src/app/instagram/page.tsx
@@ -189,7 +189,7 @@ export default function InstagramShortCodesPage() {
 						</span>
 						{status && (
 							<span className={styles.loading_text}>
-								{status.current} / {status.total}
+								{status.current + 1} / {status.total}
 							</span>
 						)}
 					</div>
